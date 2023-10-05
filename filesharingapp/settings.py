9d4a4422ad03a95f4366ef267b3457aa80e4f5cd
Ankit Tyagi <ancorp007@gmail.com>
2023-10-05 16:45:57 +0530

    handling some issues

--- filesharingapp/settings.py
+++ filesharingapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k(^kou%sy3#&n$p&-tt4rj^f!+6ym91uu8t@_200bc!*xw6n1e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','dropit-files.herokuapp.com']
 
